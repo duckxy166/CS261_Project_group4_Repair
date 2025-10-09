0fd054901a843d0855fad4144b9e0c49717dbab7
puuueiei <pukunggamer123@gmail.com>
2025-10-09 21:56:57 +0700

    Add upload file system

--- src/main/java/com/example/demo/repository/ReportRepository.java
+++ src/main/java/com/example/demo/repository/ReportRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.*;
+import com.example.demo.model.RepairRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import jakarta.persistence.*;
+
 
 @Repository
 public interface ReportRepository extends JpaRepository<RepairRequest, Long> {
